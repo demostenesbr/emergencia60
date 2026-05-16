c9ac65fdc257a5703581087a8c0b9c06a37cbbb1
Demóstenes Muniz Brito <demostenes@demostenes.com.br>
2026-05-15 22:15:08 -0300

    Refactor: Separating frontend, backend and mobile on different structures

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,20 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-
 import { ValidationPipe } from '@nestjs/common';
-
 import { AppModule } from './app.module';
-
 import { SwaggerModule } from '@nestjs/swagger';
-
 import { swaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
-
   app.enableCors();
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -24,9 +18,7 @@ async function bootstrap() {
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-
   SwaggerModule.setup('docs', app, document);
-
   await app.listen(3000);
 }
 
